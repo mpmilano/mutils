d833ae8481a605bb9a147bc00d695207e439d694
Matthew Milano <milano@cs.cornell.edu>
2016-08-18 01:44:16 -0400

    about time i had a split

--- mutils.hpp
+++ mutils.hpp
@@ -431,6 +431,13 @@ namespace mutils{
 		
 		ReassignableReference(T& t):t(&t){}
 	};
+
+	std::vector<std::string> split(const std::string &s, char c, std::size_t pos = 0){
+		auto index = s.find(c,pos);
+		auto ret = split(s,c,index+1);
+		ret.push_front(s.substr(pos,index));
+		return ret;
+	}
 	
 }
 
