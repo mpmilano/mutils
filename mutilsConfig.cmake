a749b71e120695011ea7d57f1e62ffca188a60f8
Edward Tremel <high.order.bits@gmail.com>
2023-07-28 11:19:09 -0400

    Fix CMakeLists to properly configure mutilsConfig.cmake
    Using ${CMAKE_INSTALL_PREFIX} in mutilsConfig.cmake doesn't work because
    that variable is not expanded when "make install" is executed, so the
    mutilsConfig.cmake that gets installed to e.g. /usr/local/lib/cmake
    still has the literal string ${CMAKE_INSTALL_PREFIX} in it. Then when
    another Cmake project uses find_package(mutils), it loads
    mutilsConfig.cmake and replaces ${CMAKE_INSTALL_PREFIX} with whatever
    its current value is for that project, even if it's some other directory
    like ~/.local/ and not the directory where mutils is installed. This
    results in the confusing error message that mutils was found, but
    libmutils.so could not be found, because Cmake is looking in the wrong
    directory after expanding ${CMAKE_INSTALL_PREFIX} in mutils_LIBRARIES.
    
    The fix is to use the configure_package_config_file command from
    CMakePackageConfigHelpers, and the variable PACKAGE_CMAKE_INSTALL_LIBDIR
    which gets expanded during make install. I copied these commands from
    Derecho's CMakeLists.txt where they work correctly for installing the
    package so that it can be used by another CMake project.

--- mutilsConfig.cmake
+++ mutilsConfig.cmake
@@ -1,2 +1,7 @@
-set(mutils_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include")
-set(mutils_LIBRARIES "-L${CMAKE_INSTALL_PREFIX}/lib -lmutils")
+@PACKAGE_INIT@
+
+set_and_check(mutils_INCLUDE_DIRS "@PACKAGE_CMAKE_INSTALL_INCLUDEDIR@")
+set(mutils_LIBRARIES "-L@PACKAGE_CMAKE_INSTALL_LIBDIR@ -lmutils")
+include("@PACKAGE_ConfigPackageLocation@/mutilsTargets.cmake")
+
+check_required_components(mutils)
